1b80a515bc1e7362715206a2aca9bc3005f20013
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-10-03 16:34:25 +0300

    ci

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,34 +1,39 @@
+def APP_NAME = "girie"
+def APP_REPO = "https://github.com/livelace/girie.git"
+def APP_VERSION = env.VERSION + '-${GIT_COMMIT_SHORT}'
+def IMAGE_TAG = env.VERSION == "master" ? "latest" : env.VERSION
+
 libraries {
     appimage {
-        source = "girie"
-        destination = 'girie-${VERSION}.appimage'
+        source = "${APP_NAME}"
+        destination = "${APP_NAME}-${APP_VERSION}.appimage"
     }
     dependency_check
     dependency_track {
-        project = "girie"
-        version = "master"
+        project = "${APP_NAME}"
+        version = env.VERSION
     }
     git {
-        repo_url = "https://github.com/livelace/girie.git"
+        repo_url = "${APP_REPO}"
+        repo_branch = env.VERSION
     }
     go {
         options = "github.com/livelace/girie/cmd/girie"
     }
     harbor_replicate {
-        policy = "girie"
+        policy = "${APP_NAME}"
     }
     k8s_build {
         image = "harbor-core.k8s-2.livelace.ru/dev/gobuild:latest"
         privileged = true
     }
     kaniko {
-        destination = "data/girie:latest"
+        destination = "data/${APP_NAME}:${IMAGE_TAG}"
     }
     mattermost
     nexus {
-        source = 'girie-${VERSION}.appimage'
-        destination = 'dists-internal/girie/girie-${VERSION}'
+        source = "${APP_NAME}-${APP_VERSION}.appimage"
+        destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.appimage"
     }
     sonarqube
-    version
 }
